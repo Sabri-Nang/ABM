2570b0f5848d302205953a2fa3e9a2ff869c3a8c
Sabrina Sanches <sabrinadelosangelessanches@gmail.com>
2024-06-20 21:35:51 -0300

    modifico base_datos

--- functions/base_datos.py
+++ functions/base_datos.py
@@ -6,25 +6,27 @@ class Base_Datos:
         self.cantidad_datos = 0
         self.datos = pd.DataFrame()
 
-    def agregar_persona(self, persona):
-        persona_df = persona.obtener_df()
-        self.datos = pd.concat([self.datos, persona_df],
+    def agregar_registro(self, registro):
+        self.cantidad_datos += 1
+        registro_df = registro.obtener_df()
+        self.datos = pd.concat([self.datos, registro_df],
                                axis=0, ignore_index=True)
 
-    def buscar_persona(self, persona_nombre):
-        if len(self.datos.query(f"name == '{persona_nombre}'")) >= 1:
-            registros = len(self.datos.query(f"name == '{persona_nombre}'"))
-            print(f'Se encontraron {registros} con el nombre {persona_nombre}')
+    def buscar_registro(self, atributo, valor):
+        if len(self.datos.query(f"{atributo} == '{valor}'")) >= 1:
+            registros = len(self.datos.query(f"{atributo} == '{valor}'"))
+            print(f'Se encontraron {registros} con el {atributo} {valor}')
             return True
-        print(f'No se encontraron registros con el nombre {persona_nombre}')
+        print(f'No se encontraron registros con el {atributo} {valor}')
         return False
 
-    def eliminar_persona(self, persona_nombre):
-        if self.buscar_persona(persona_nombre):
-            datos_filtrados = self.datos[self.datos['name'] != persona_nombre]
-            for index, row in self.datos[self.datos['name'] == persona_nombre].iterrows():
+    def eliminar_registro(self, atributo, valor):
+        if self.buscar_registro(atributo, valor):
+            datos_filtrados = self.datos[self.datos[f'{atributo}'] != valor]
+            for index, row in self.datos[self.datos[f'{atributo}'] == valor].iterrows():
                 print(f'Se elimino la fila {index}: {self.datos.iloc[index]}')
             self.datos = datos_filtrados
+            self.cantidad_datos = self.size
             return True
         return False
     
