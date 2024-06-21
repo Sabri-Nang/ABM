2df7925cf03a0ed1e4e9a9cf1554cadd34f2e8c5
Sabrina Sanches <sabrinadelosangelessanches@gmail.com>
2024-06-21 09:49:13 -0300

    agrego crear_tabla_empleados

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from functions.functions_solicitante import ingresar_solicitante
+from functions.functions_empleado import crear_tabla_empleados
 from functions.abm_menu import mensaje_bienvenida, ingresar_id
 from functions.abm_menu import mostrar_empleado
-from settings.settings import base_datos, servidor
-from functions.base_datos_sqlserver import crear_base_datos
 from functions.crear_tablas_db import crear_tablas
 
 
@@ -10,8 +9,8 @@ def main():
     '''
     Funcion principal
     '''
-    crear_base_datos(servidor, base_datos)
     crear_tablas()
+    crear_tabla_empleados()
     while True:
         opcion = mensaje_bienvenida()
         if opcion == 1:
@@ -21,6 +20,9 @@ def main():
         elif opcion == 3:
             mostrar_empleado()
         elif opcion == 4:
+            print('-'*30)
+            print('Gracias por utilizar nuestros servicios')
+            print('Cerrando el sistema...')
             exit()
 
 
