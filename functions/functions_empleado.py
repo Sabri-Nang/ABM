765949e13615055ef6762bd5483e3d5ec9793303
Sabrina Sanches <sabrinadelosangelessanches@gmail.com>
2024-06-20 21:36:24 -0300

    modifico estados

--- functions/functions_empleado.py
+++ functions/functions_empleado.py
@@ -6,7 +6,7 @@ def cambiar_estado_tramite() -> str: #Para Empleado
     Cambia el estado del trámite.
     Devuelve el estado seleccionado.
     '''
-    estados = {1: 'iniciado', 2: 'en proceso', 3: 'finalizado'}
+    estados = {1: 'iniciado', 2: 'en proceso', 3: 'resuelto'}
     print('Seleccione el estado del trámite')
     while True:
         for k, v in estados.items():
@@ -14,6 +14,7 @@ def cambiar_estado_tramite() -> str: #Para Empleado
         try:
             estado_seleccionado = int(input('Ingrese el número: '))
             if estado_seleccionado not in [1, 2, 3]:
+                print('El valor ingresado no es válido')
                 continue
             break
         except ValueError:
