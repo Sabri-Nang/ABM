99e87edc8cf12b0351d5e98f321eaf365aef0f8c
Sabrina Sanches <sabrinadelosangelessanches@gmail.com>
2024-06-21 08:19:12 -0300

    agrego crear_tablas_db

---
+++ functions/crear_tablas_db.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from settings.settings import servidor, base_datos, tabla_empleados, tabla_tramites
+from functions.base_datos_sqlserver import crear_base_datos
+
+# Cadena de conexión a la base de datos específica
+connection_string = f"mssql+pyodbc://{servidor}/{base_datos}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+engine = create_engine(connection_string)
+
+# Definir la estructura de la tabla utilizando SQLAlchemy
+Base = declarative_base()
+
+
+class Tramites(Base):
+    __tablename__ = tabla_tramites
+    id_tramite = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String, nullable=True)
+    apellido = Column(String, nullable=True)
+    telefono = Column(Integer, nullable=True)
+    mail = Column(String, nullable=True)
+    DNI = Column(String, nullable=False)
+    tramite = Column(String, nullable=False)
+    estado = Column(String, nullable=False)
+    horario_solicitud = Column(DateTime, nullable=False)
+
+
+class Empleados(Base):
+    __tablename__ = tabla_empleados
+    id_empleado = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String, nullable=True)
+    apellido = Column(String, nullable=True)
+    telefono = Column(Integer, nullable=True)
+    mail = Column(String, nullable=True)
+    DNI = Column(String, nullable=False)
+    puesto = Column(String, nullable=False)
+    
+
+def crear_tablas():
+    # Crear las tablas en la base de datos
+    crear_base_datos(servidor, base_datos)
+    Base.metadata.create_all(engine)
+    print(f"Se ha creado la tabla {tabla_tramites} en la base de datos {base_datos}.")
+    print(f"Se ha creado la tabla {tabla_empleados} en la base de datos {base_datos}")
+
+
